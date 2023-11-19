6aea04e0386c3c15b7a893539e939dc69c408e2c
Fahim <fahimshahariar.fs@gmail.com>
2023-11-19 12:41:13 +0600

    startup

---
+++ lib/common/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors{
+
+ static const Color buttonBg = Color(0xff1c1c1c);
+
+ static const Color buttonColor = Color(0xff80e470);
+ static const Color secondbuttonColor = Color(0xff7b7b7b);
+ static const Color fieldbg = Color(0xff2e2e2e);
+
+}
---
+++ lib/startuppage.dart
@@ -0,0 +1,79 @@
+import 'package:edtechapp/common/colors.dart';
+import 'package:edtechapp/loginSignup/login/login.dart';
+import 'package:flutter/material.dart';
+
+class StartUp extends StatefulWidget {
+  const StartUp({super.key});
+
+  @override
+  State<StartUp> createState() => _StartUpState();
+}
+
+class _StartUpState extends State<StartUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("assets/images/startbg.png"),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 30),
+              height: 80,
+              width: 300,
+              decoration: BoxDecoration(
+                color: AppColors.buttonBg,
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 5),
+                    width: 75,
+                    height: 75,
+                    decoration: BoxDecoration(
+                        color: AppColors.fieldbg,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: const Icon(
+                      Icons.arrow_back,
+                      color: AppColors.secondbuttonColor,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 5),
+                    width: 75,
+                    height: 75,
+                    decoration: BoxDecoration(
+                      color: AppColors.buttonColor,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Login(),
+                          ),
+                        );
+                      },
+                      child: Icon(Icons.arrow_forward),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
