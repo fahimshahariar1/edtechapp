7cf2fa11823b99817884658a3d44111be1b67ff3
Fahim <fahimshahariar.fs@gmail.com>
2023-11-21 14:05:47 +0600

    controller

---
+++ elearningapp1/lib/pages/login/loginwidgets/signinwidget.dart
@@ -0,0 +1,93 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../common/values/colors.dart';
+import '../signin.dart';
+
+Widget reusableIcon(String iconname, String icontype) {
+  return GestureDetector(
+    onTap: () {
+
+    },
+    child: Center(
+      child: Container(
+        margin: EdgeInsets.all(5),
+        height: 30.h,
+        width: 300.w,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: icontype=="Email"?AppColors.buttonColor:AppColors.iconbg,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+         Row(
+           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+           children: [
+             Image.asset(width: 20,height: 20,"assets/icons/$iconname.png" ),
+             Text("   Login With $iconname", style: TextStyle(color: icontype=="Email"?AppColors.buttonBg:AppColors.fontcolor, fontWeight: FontWeight.bold),),
+           ],
+         )
+        ]),
+      ),
+    ),
+  );
+}
+
+Widget reusableTexts(String text) {
+  return Container(
+    margin: EdgeInsets.only(bottom: 5.h),
+    child: Text(text,style: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: AppColors.fontcolor
+    ),),
+  );
+}
+
+Widget buildTextfield(String hinttext, String textType,
+
+    void Function(String value)? func
+
+    ) {
+  return Container(
+    padding: EdgeInsets.only(left: 20, right: 20, bottom: 2),
+    margin: EdgeInsets.all(10),
+    height: 40.h,
+  width: 300.h,
+  decoration: BoxDecoration(
+    color: AppColors.iconbg,
+        borderRadius: BorderRadius.circular(40),
+  ),
+  child: TextField(
+    onChanged:  (value)=>func!(value) ,
+    decoration: InputDecoration(
+      hintText: hinttext, hintStyle: TextStyle(
+      color: AppColors.fontcolor2
+    )
+    ),
+  )
+  );
+}
+
+
+Widget buildButton(String buttonName, void Function()? func){
+  return GestureDetector(
+    onTap: (){
+      func;
+    },
+    child: Container(
+
+      height: 30.h,
+      width: 300.w,
+      decoration: BoxDecoration(
+        color: AppColors.iconbg,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Center(child: Text(buttonName)),
+
+    ),
+  );
+}
---
+++ elearningapp1/lib/pages/login/signinController.dart
@@ -0,0 +1,60 @@
+import 'package:elearningapp1/common/widgets/flutterToast.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/signinBLocs.dart';
+
+class SignInController {
+  final BuildContext context;
+
+  SignInController(this.context, {required BuildContext context});
+
+  Future<void> handleSignIn(String type) async {
+    try {
+      if (type == "email") {
+        final state = context.read<SignInBloc>().state;
+        String emailAddress = state.email;
+        String pass = state.pass;
+        if (emailAddress.isEmpty) {
+          toastInfo(msg: "Email Is Empty");
+          return;
+        }
+        if (pass.isEmpty) {
+          toastInfo(msg: "Password Is Empty");
+          return;
+        }
+
+        try {
+          final credential = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(email: emailAddress, password: pass);
+          if (credential.user == null) {
+            toastInfo(msg: "User Does Not Exist");
+            return;
+          }
+          if (!credential.user!.emailVerified) {
+            toastInfo(msg: "Verify Your Email");
+            return;
+          }
+          var user = credential.user;
+          if (user != null) {
+          } else {
+            toastInfo(msg: "Not a User");
+            return;
+          }
+        }
+        on FirebaseAuthException catch (e) {
+          if (e.code == "user-not-found") {
+            toastInfo(msg: "User Not Found");
+          }
+          else if (e.code == "wrong-password") {
+            toastInfo(msg: "Wrong Password");
+          }
+          else if (e.code == "invalid-email") {
+            toastInfo(msg: "Invalid Email");
+          }
+        }
+      }
+    } catch (e) {}
+  }
+}
